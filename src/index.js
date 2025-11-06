8b2bb789a8321d78a36a1aeb8d90e516077919c4
Bjoern Koneswarakantha <erblast2001@gmx.de>
2025-11-06 09:06:44 +0100

    fix: make index.js gitignore pattern root-specific to include src/index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * gsm.simaerep.viz - Visualization library for simaerep data
+ * Main entry point
+ */
+
+import SiteList from './SiteList.js';
+
+export default {
+  SiteList,
+};
+
+export { SiteList };
+
